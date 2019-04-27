610ac50bc62fbc0abf47960bc79ffa9ca55184be
mjbenefiel <michaeljbenefiel@gmail.com>
2019-04-26 21:48:42 -0700

    04262019 update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `My personal blog`,
+    title: `Notes and more`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
